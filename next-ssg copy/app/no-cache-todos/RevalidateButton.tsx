920a385869245261b814f53cf9c8eae5eefcd423
Mh-alim <alim.khan@moneyview.in>
2024-11-30 11:06:55 +0530

    file location moved

--- next-ssg copy/app/no-cache-todos/RevalidateButton.tsx
+++ next-ssg copy/app/no-cache-todos/RevalidateButton.tsx
@@ -6,7 +6,7 @@ import Page from "./page";
 export const RevalidateButton = () => {
   const handleRevalidate = async () => {
     // Call the server action to revalidate the cache
-    const revalidate = (await import("@/app/lib/actions/action1")).default;
+    const revalidate = (await import("@/lib/actions/action1")).default;
     await revalidate();
     alert("Cache revalidated!");
   };
